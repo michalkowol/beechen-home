22b7dbf4a6f44d3ebc364d3f2250b9f47fd76224
Michal Kowol <michal.kowol@vimn.com>
2019-11-23 17:32:43 +0100

    Use standard plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,27 @@
 plugins {
     java
-    id("tech.viacom.neutron.neutron-java") version "0.0.1_build21"
-    id("tech.viacom.neutron.neutron-spring-boot") version "0.0.1_build21"
+    id("org.springframework.boot") version "2.1.7.RELEASE"
 }
+
+apply(plugin = "io.spring.dependency-management")
 
 group = "com.michalkowol"
 version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
 
 dependencies {
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 
-    implementation("com.github.spotbugs:spotbugs-annotations")
+    implementation("com.github.spotbugs:spotbugs-annotations:3.1.12")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+}
+
+tasks.wrapper {
+    gradleVersion = "5.6.2"
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,1 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven {
-            name = "mtvn-proximity"
-            url = uri("https://nexus.mtvi.com/nexus/repository/releases")
-            credentials {
-                val nexusUser: String? by settings
-                val nexusPassword: String? by settings
-                username = System.getenv("NEXUS_USERNAME") ?: System.getenv("NEXUS_USER") ?: nexusUser
-                password = System.getenv("NEXUS_PASSWORD") ?: System.getenv("NEXUS_PASS") ?: nexusPassword
-            }
-        }
-    }
-}
-
 rootProject.name = "beechen-home"
